0d93c033b7fc70ed68a1b163c8088fd03f9af3e0
Veaceslav Diuta <veaceslavdiuta.dev@gmail.com>
2025-02-14 14:56:30 +0200

    Create Pricing page and add Pricing Plans section to it

---
+++ src/routes/Pricing.jsx
@@ -0,0 +1,12 @@
+import PricingPlans from '../components/PricingPlans';
+
+function Pricing() {
+  return (
+    <main className="flex flex-col">
+      <PricingPlans />
+      <section></section>
+    </main>
+  );
+}
+
+export default Pricing;
