3323fbc55553c687a1fa2c13076cc25352e9cdb4
Veaceslav Diuta <veaceslavdiuta.dev@gmail.com>
2025-02-15 19:21:48 +0200

    Create Portfolio page and add a Portfolio Hero section to it

---
+++ src/routes/Portfolio.jsx
@@ -0,0 +1,13 @@
+import PortfolioHero from '../components/PortfolioHero';
+
+function Portfolio() {
+  return (
+    <main>
+      <PortfolioHero />
+      <section></section>
+      <section></section>
+    </main>
+  );
+}
+
+export default Portfolio;
