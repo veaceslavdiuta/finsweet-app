227afa8904d967a58d5ba5a2eb8b97e33acc4e55
Veaceslav Diuta <veaceslavdiuta.dev@gmail.com>
2025-02-17 15:01:03 +0200

    Create Blog page and add Featured Blog section to it

---
+++ src/routes/Blog.jsx
@@ -0,0 +1,12 @@
+import FeaturedBlog from '../components/FeaturedBlog';
+
+function Blog() {
+  return (
+    <main className="flex flex-col">
+      <FeaturedBlog />
+      <section></section>
+    </main>
+  );
+}
+
+export default Blog;
