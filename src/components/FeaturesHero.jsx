a47f5d42a3b67da90428a5a14a1ea13b683ff5d8
Veaceslav Diuta <veaceslavdiuta.dev@gmail.com>
2025-02-24 14:17:24 +0200

    Implement responsive design for Features page

--- src/components/FeaturesHero.jsx
+++ src/components/FeaturesHero.jsx
@@ -2,24 +2,24 @@ import { Link } from 'react-router-dom';
 
 function FeaturesHero() {
   return (
-    <section className="flex w-full justify-center bg-tint-blue py-32">
-      <div className="flex w-full max-w-screen-xl gap-16">
-        <div className="flex w-1/2 flex-col items-start gap-8">
-          <h1 className="w-[80%] font-poppins text-6xl font-semibold leading-snug text-white">
+    <section className="w-full bg-tint-blue px-8 py-20 lg:py-32 2xl:px-0">
+      <div className="container mx-auto flex max-w-screen-xl flex-wrap items-center justify-center gap-8 lg:flex-nowrap lg:gap-16">
+        <div className="flex flex-col items-center gap-8 lg:w-1/2 lg:items-start">
+          <h1 className="text-center font-poppins text-4xl font-semibold leading-snug text-white sm:text-5xl lg:w-[80%] lg:text-start lg:text-6xl">
             All the features you need
           </h1>
-          <p className="w-[85%] font-poppins text-base font-medium leading-7 text-white opacity-70">
+          <p className="text-center font-poppins text-sm font-medium leading-7 text-white opacity-70 sm:text-base lg:w-[85%] lg:text-start">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
           <Link
-            className="rounded-custom-41 bg-yelow px-12 py-4 font-poppins text-lg font-semibold leading-8 text-dark-blue hover:bg-hover-yelow"
+            className="rounded-custom-41 bg-yelow px-6 py-2 font-poppins text-base font-semibold leading-8 text-dark-blue hover:bg-hover-yelow sm:px-12 sm:py-4 sm:text-lg"
             to="/pricing"
           >
             View Pricing
           </Link>
         </div>
-        <div className="w-1/2">
+        <div className="lg:w-1/2">
           <img src="/assets/featuresHero.png" alt="featuresHero" />
         </div>
       </div>
