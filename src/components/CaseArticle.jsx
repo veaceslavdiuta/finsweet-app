b09395f0af0d2b16901afe0e707cc17ffd592281
Veaceslav Diuta <veaceslavdiuta.dev@gmail.com>
2025-02-26 11:59:50 +0200

    Implement responsive design for Read Case and Read Blog pages

--- src/components/CaseArticle.jsx
+++ src/components/CaseArticle.jsx
@@ -1,19 +1,19 @@
 function CaseArticle(props) {
   return (
-    <section className="flex w-full justify-center">
-      <div className="flex w-[840px] flex-col gap-16 pb-28">
-        <div className="flex flex-col gap-6">
-          <h3 className="font-poppins text-4xll font-semibold leading-normal text-dark-blue">
+    <section className="w-full px-8">
+      <div className="container mx-auto flex flex-col gap-16 pb-20 lg:w-[840px] lg:pb-28">
+        <div className="flex flex-col gap-3 md:gap-6">
+          <h3 className="font-poppins text-3xl font-semibold leading-normal text-dark-blue sm:text-4xll">
             About the project
           </h3>
-          <p className="font-poppins text-base font-normal leading-7 text-dark-blue opacity-70">
+          <p className="font-poppins text-sm font-normal leading-7 text-dark-blue opacity-70 sm:text-base">
             {props.aboutCase}
           </p>
           <ul className="flex list-disc flex-col gap-4 pl-[18px]">
             {props.aboutCaseSteps.map((step, index) => (
               <li
                 key={index}
-                className="font-poppins text-base font-normal leading-7 text-dark-blue opacity-70"
+                className="font-poppins text-sm font-normal leading-7 text-dark-blue opacity-70 sm:text-base"
               >
                 {step}
               </li>
@@ -21,25 +21,29 @@ function CaseArticle(props) {
           </ul>
         </div>
         <div
-          className="px-14 py-11"
+          className="p-6 lg:px-14 lg:py-11"
           style={{
             background: props.templateBackground,
           }}
         >
-          <img src="/assets/readCaseImg2.png" alt="imageCase" />
+          <img
+            src="/assets/readCaseImg2.png"
+            className="h-full w-full"
+            alt="imageCase"
+          />
         </div>
-        <div className="flex flex-col gap-6">
-          <h3 className="font-poppins text-4xll font-semibold leading-normal text-dark-blue">
+        <div className="flex flex-col gap-3 md:gap-6">
+          <h3 className="font-poppins text-3xl font-semibold leading-normal text-dark-blue sm:text-4xll">
             How we do it
           </h3>
-          <p className="font-poppins text-base font-normal leading-7 text-dark-blue opacity-70">
+          <p className="font-poppins text-sm font-normal leading-7 text-dark-blue opacity-70 sm:text-base">
             {props.howWeDoCase}
           </p>
           <ul className="flex list-disc flex-col gap-4 pl-[18px]">
             {props.howWeDoCaseSteps.map((step, index) => (
               <li
                 key={index}
-                className="font-poppins text-base font-normal leading-7 text-dark-blue opacity-70"
+                className="font-poppins text-sm font-normal leading-7 text-dark-blue opacity-70 sm:text-base"
               >
                 {step}
               </li>
--- src/routes/ReadBlog.jsx
+++ src/routes/ReadBlog.jsx
@@ -12,14 +12,14 @@ function ReadBlog() {
   }, [id]);
 
   return (
-    <main className="container mx-auto max-w-screen-xl py-32">
+    <main className="container mx-auto max-w-screen-xl px-8 py-20 lg:py-32 2xl:px-0">
       {post && (
         <div className="flex flex-col items-center gap-20">
-          <div className="flex flex-col items-center gap-6">
-            <h1 className="w-[75%] text-center font-poppins text-5xl font-semibold leading-snug text-dark-blue">
+          <div className="flex flex-col items-center gap-3 md:gap-6">
+            <h1 className="text-center font-poppins text-3xl font-semibold leading-snug text-dark-blue sm:text-4xl md:text-5xl lg:w-[75%]">
               {post.postName}
             </h1>
-            <h5 className="flex gap-4 font-poppins text-base font-medium leading-7 text-dark-blue">
+            <h5 className="flex gap-4 font-poppins text-sm font-medium leading-7 text-dark-blue sm:text-base">
               <span className="opacity-85">{post.postAuthor}</span>
               Posted on {post.postData}
             </h5>
@@ -29,37 +29,37 @@ function ReadBlog() {
               alt="FeaturedBlog"
             />
           </div>
-          <div className="flex w-[840px] flex-col gap-8">
-            <h3 className="font-poppins text-4xll font-semibold leading-normal text-dark-blue">
+          <div className="flex flex-col gap-4 md:gap-8 lg:w-[840px]">
+            <h3 className="font-poppins text-3xl font-semibold leading-normal text-dark-blue sm:text-4xll">
               {post.postTitle}
             </h3>
-            <p className="font-poppins text-base font-normal leading-7 text-dark-blue opacity-70">
+            <p className="font-poppins text-sm font-normal leading-7 text-dark-blue opacity-70 sm:text-base">
               {post.postTitleDescr}
             </p>
-            <h3 className="font-poppins text-4xll font-semibold leading-normal text-dark-blue">
+            <h3 className="font-poppins text-3xl font-semibold leading-normal text-dark-blue sm:text-4xll">
               {post.aboutPost}
             </h3>
-            <p className="font-poppins text-base font-normal leading-7 text-dark-blue opacity-70">
+            <p className="font-poppins text-sm font-normal leading-7 text-dark-blue opacity-70 sm:text-base">
               {post.aboutPostDescr}
             </p>
             <ul className="flex list-disc flex-col gap-4 pl-[18px]">
               {post.aboutPostSteps.map((step, index) => (
                 <li
                   key={index}
-                  className="font-poppins text-base font-normal leading-7 text-dark-blue opacity-70"
+                  className="font-poppins text-sm font-normal leading-7 text-dark-blue opacity-70 sm:text-base"
                 >
                   {step}
                 </li>
               ))}
             </ul>
-            <p className="font-poppins text-base font-normal leading-7 text-dark-blue opacity-70">
+            <p className="font-poppins text-sm font-normal leading-7 text-dark-blue opacity-70 sm:text-base">
               {post.aboutPostFin}
             </p>
             <img src="/assets/readBlog2.jpg" alt="blog-image" />
-            <h3 className="font-poppins text-4xll font-semibold leading-normal text-dark-blue">
+            <h3 className="font-poppins text-3xl font-semibold leading-normal text-dark-blue sm:text-4xll">
               {post.postConclusionTitle}
             </h3>
-            <p className="font-poppins text-base font-normal leading-7 text-dark-blue opacity-70">
+            <p className="font-poppins text-sm font-normal leading-7 text-dark-blue opacity-70 sm:text-base">
               {post.postConclusionDescr}
             </p>
           </div>
